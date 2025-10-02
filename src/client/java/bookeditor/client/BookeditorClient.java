7607d0157172d7fee07c8863a48566527b59ed0f
Iliias Bayishev <168758422+Iliiasik@users.noreply.github.com>
2025-10-02 22:22:00 +0600

    init commit

---
+++ src/client/java/bookeditor/client/BookeditorClient.java
@@ -0,0 +1,10 @@
+package bookeditor.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BookeditorClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/bookeditor/Bookeditor.java
@@ -0,0 +1,10 @@
+package bookeditor;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Bookeditor implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
