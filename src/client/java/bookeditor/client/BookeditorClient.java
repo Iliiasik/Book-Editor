921272a959bc2af2a69e141ed9842cec0a693b38
Iliias Bayishev <168758422+Iliiasik@users.noreply.github.com>
2025-10-04 19:44:16 +0600

    Textures

--- src/client/java/bookeditor/client/BookeditorClient.java
+++ src/client/java/bookeditor/client/BookeditorClient.java
@@ -5,8 +5,12 @@ import bookeditor.data.BookData;
 import bookeditor.platform.Services;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.item.ModelPredicateProviderRegistry;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
 
 public class BookeditorClient implements ClientModInitializer {
     @Override
@@ -17,5 +21,15 @@ public class BookeditorClient implements ClientModInitializer {
             BookData.ensureDefaults(stack, mc.player);
             mc.setScreen(new WysiwygBookScreen(stack, hand));
         };
+
+        Item creativeBook = Registries.ITEM.get(new Identifier("bookeditor", "creative_book"));
+        ModelPredicateProviderRegistry.register(
+                creativeBook,
+                new Identifier("bookeditor", "signed"),
+                (stack, world, entity, seed) -> {
+                    BookData d = BookData.readFrom(stack);
+                    return d.signed ? 1.0f : 0.0f;
+                }
+        );
     }
 }
